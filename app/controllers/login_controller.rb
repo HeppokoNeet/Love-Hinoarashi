402becb09c9e1e4d8b878ee552a4b1d25fd1fad5
HeppokoNeet <tatsuru.daime@i-standard.jp>
2021-10-27 22:50:30 +0900

    ログインできた

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,6 +2,15 @@ class LoginController < ApplicationController
   def index; end
 
   def create
-    binding.pry
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      view_context.log_in(user)
+      redirect_to root_path, notice: 'ログインしました'
+    else
+      login_error_text = 'メールアドレスまたはパスワードに誤りがあります'
+      flash[:error] = login_error_text
+      render 'login/index', layout: 'noHeaderFooter.html.slim'
+      return
+    end
   end
 end
