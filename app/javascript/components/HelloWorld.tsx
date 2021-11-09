1d99c79be68b70d79034ef41b016c453ab5c2502
HeppokoNeet <tatsuru.daime@i-standard.jp>
2021-11-09 21:44:54 +0900

    save

--- app/javascript/components/HelloWorld.tsx
+++ app/javascript/components/HelloWorld.tsx
@@ -1,21 +1,15 @@
-import * as React from "react"
-
+import * as React from "react";
 
 interface IHelloWorldProps {
   greeting: string;
 }
 
-interface IHelloWorldState {
-}
+interface IHelloWorldState {}
 
-class HelloWorld extends React.Component <IHelloWorldProps, IHelloWorldState> {
+class HelloWorld extends React.Component<IHelloWorldProps, IHelloWorldState> {
   render() {
-    return (
-      <React.Fragment>
-        Greeting: {this.props.greeting}
-      </React.Fragment>
-    );
+    return <React.Fragment>Greeting: {this.props.greeting}</React.Fragment>;
   }
 }
 
-export default HelloWorld
+export default HelloWorld;
